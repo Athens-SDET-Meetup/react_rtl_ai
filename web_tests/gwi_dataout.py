9242b5a06dbbbc36893b8776bfa63c1db0acc7d5
Giannis Papadakis <gpapadakis@gwi.com>
2024-10-15 09:09:15 +0300

    wip

--- web_tests/gwi_dataout.py
+++ web_tests/gwi_dataout.py
@@ -22,7 +22,7 @@ driver.get('https://signin-staging.globalwebindex.com')
 # Wait for the page to load
 time.sleep(2)
 
-target = driver.find_element(By.ID,'email-validation')
+target = driver.find_element(By.ID,'email')
 
 # Scrape data
 elements_attributes = driver.find_elements(By.TAG_NAME,'input')
@@ -31,19 +31,19 @@ attributes = []
 for element in elements_attributes:
     highlight(element, 3, "blue", 5)
     if element.get_attribute(By.ID) not in [None, '']:
-        element_type = element.get_attribute('type')
+        element_type = element.get_attribute('data-testid')
         element_id = element.get_attribute(By.ID)
-        element_name = element.get_attribute(By.NAME)
-        attributes.append({element_type + "_element", element_id, element_name})
-
+        element_name = element.get_attribute('placeholder')
+        attributes.append({element_type, element_id, element_name})
+        print (attributes)
 # adding header 
 headerList = ['element', 'id', 'name']     
 
 target_attributes = []
-target_element_type = target.get_attribute('type')
+target_element_type = target.get_attribute('data-testid')
 target_element_id = target.get_attribute(By.ID)
-target_element_name = target.get_attribute(By.NAME)
-target_attributes.append({target_element_type + "_element", target_element_id, target_element_name})
+target_element_name = target.get_attribute('placeholder')
+target_attributes.append({target_element_type, target_element_id, target_element_name})
 
 # Print the scraped data
 import pandas as pd
